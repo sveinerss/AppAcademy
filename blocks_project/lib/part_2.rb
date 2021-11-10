e95a2a71aaa78ee34aa4998033a3549ffe1b87b7
Svein Gonzalez <42025111+sveinerss@users.noreply.github.com>
2021-11-09 17:35:01 -0800

    completed blocks

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -4,5 +4,12 @@ def all_words_capitalized? (arr)
 end
 
 def no_valid_url? (arr)
-    arr.none
+    bad_urls = [".com",".net",".io",".org"]
+    arr.none? {|url| url.include?(".com") ||url.include?(".net")||url.include?(".io")||url.include?(".org")}
+end
+
+def any_passing_students? (arr)
+    arr.any? do |student| 
+       (student [:grades].sum / student [:grades].length) >= 75
+    end
 end
